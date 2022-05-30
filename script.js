cbb9cfde07ca31b755744314f84b5b83c5c3a232
Angel Prado <angel_joaquin12@outlook.com>
2022-05-30 04:35:09 -0500

    numbers and operators function

--- script.js
+++ script.js
@@ -1,22 +1,73 @@
-let firstInput = document.querySelector('#firstNumber');
-// let secondInput = document.querySelector('#secondNumber');
-let addBtn = document.querySelector('#add');
-let subBtn = document.querySelector('#subtract');
-let mulBtn = document.querySelector('#multiply');
-let divBtn = document.querySelector('#divide');
-let equBtn = document.querySelector('#equals');
-let oneBtn = document.querySelector('#one');
-let twoBtn = document.querySelector('#two');
-let threeBtn = document.querySelector('#three');
-let fourBtn = document.querySelector('#four');
-let operatorButtons = document.getElementsByClassName('operatorButtons');
-// operatorButtons.addEventListener('click', ()=> console.log("I am working."))
+let firstInput = document.querySelector("#firstNumber");
+let equBtn = document.querySelector("#equals");
+let numButtons = document.querySelectorAll(".numButtons");
+let operatorButtons = document.querySelectorAll(".operatorButtons");
+let clearButton = document.getElementById("clearButton");
 
-// for (var i=0; i < operatorButtons.length; i++) {
-//     operatorButtons[i].addEventListener('click', ()=> console.log(80+ 90));
-// }
-// console.log(10 + 80)
-[document.querySelector('#add'), document.querySelector('#subtract'), document.querySelector('#multiply'), 
-document.querySelector('#divide')].forEach(element => {
-    element.addEventListener('click', ()=> console.log(((80) + element.value + (80))));
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+let number = "";
+let expression = "";
+
+numButtons.forEach((numberValue) => {
+  numberValue.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("e parameter", e.target.value);
+    if (firstNumber != "" && operator != "") {
+      secondNumber = secondNumber + e.target.value;
+      expression = operator + secondNumber;
+      document.getElementById("numberBox").value = expression;
+    } else {
+      firstNumber = firstNumber + e.target.value;
+      document.getElementById("numberBox").value = firstNumber;
+      console.log(firstNumber);
+    }
+  });
 });
+operatorButtons.forEach((operatorValue) => {
+  operatorValue.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("e parameter", e.target.value);
+    const op = e.target.value;
+    if (firstNumber != "" && op != "" && secondNumber != "") {
+      firstNumber = evaluation();
+      operator = firstNumber + op;
+      secondNumber = "";
+      console.log(operator);
+    } else {
+      operator = firstNumber + op;
+      document.getElementById("numberBox").value = operator.toFixed(3);
+      console.log(operator);
+    }
+  });
+});
+equBtn.addEventListener("click", (e) => {
+  var newResult = evaluation();
+  firstNumber = newResult;
+  document.getElementById("numberBox").value = newResult.toFixed(3);
+  secondNumber = "";
+});
+
+function evaluation() {
+  var firstInteger = parseFloat(expression);
+  var firstIntegerString = firstInteger.toString();
+  var firstIntegerStringlength = firstIntegerString.length;
+  let slicedExpression = expression.slice(firstIntegerStringlength);
+  var newOperator = slicedExpression[0];
+  var nextNumber = slicedExpression.slice(1);
+  var secondInteger = parseFloat(nextNumber);
+  let result = 0;
+  if (newOperator == "+") {
+    result = firstInteger + secondInteger;
+    console.log();
+  } else if (newOperator == "-") {
+    result = firstInteger - secondInteger;
+  } else if (newOperator == "*") {
+    result = firstInteger * secondInteger;
+  } else if (newOperator == "/") {
+    result = firstInteger / secondInteger;
+  }
+
+  return result;
+}
