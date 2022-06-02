1ba99467be1c2ddd927250ea292dcf4193a8319f
Angel Prado <angel_joaquin12@outlook.com>
2022-06-01 23:03:34 -0500

    Flex buttons

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let firstInput = document.querySelector("#firstNumber");
 let equBtn = document.querySelector("#equals");
 let numButtons = document.querySelectorAll(".numButtons");
 let operatorButtons = document.querySelectorAll(".operatorButtons");
-let clearButton = document.getElementById("clearButton");
-let memory = document.getElementById("memRecall");
+let display = document.getElementById("numberBox");
+let clearDisplayButton = document.getElementById("clearButton");
+let periodButton = document.getElementById("period");
+let memRecall = document.getElementById("memRecall");
 let storageClear = document.getElementById("storageClear");
 let storageSave = document.getElementById("storageSave");
 let firstNumber = "";
@@ -12,74 +14,92 @@ let secondNumber = "";
 let number = "";
 let expression = "";
 
-memory.addEventListener('click',(e)=> {
-    var savedNumber = localStorage.getItem('newResult');
-    document.getElementById("numberBox").value = savedNumber;
-    console.log(savedNumber);
-})
+memRecall.addEventListener("click", resultMemorySave);
 
-storageClear.addEventListener('click', (e) => {
-    localStorage.clear();
-})
+storageClear.addEventListener("click", clearStorage);
 
-// storageSave.addEventListener('click', (e) => {
-//     localStorage.setItem("newResult", document.getElementById('numberBox').value)
-// })
+storageSave.addEventListener("click", (e) => {
+  localStorage.setItem("Memory", document.getElementById("numberBox").value);
+});
+
+clearDisplayButton.addEventListener("click", (e) => {
+  document.getElementById("numberBox").innerHTML = "0";
+});
 
 numButtons.forEach((numberValue) => {
-  numberValue.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("e parameter", e.target.value);
-    if (firstNumber != "" && operator != "") {
-      secondNumber = secondNumber + e.target.value;
-      expression = operator + secondNumber;
-      document.getElementById("numberBox").value = expression;
-    } else {
-      firstNumber = firstNumber + e.target.value;
-      document.getElementById("numberBox").value = firstNumber;
-      console.log(firstNumber);
-    }
-  });
+  numberValue.addEventListener("click", addingNumbersToCalculator);
 });
+
 operatorButtons.forEach((operatorValue) => {
-  operatorValue.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("e parameter", e.target.value);
-    const op = e.target.value;
-    if (firstNumber != "" && op != "" && secondNumber != "") {
-      firstNumber = evaluation().toFixed(3);
-      operator = firstNumber + op;
-      secondNumber = "";
-      console.log(operator);
-    } else {
-      operator = firstNumber + op;
-      document.getElementById("numberBox").value = operator;
-      console.log(operator);
-    }
-    
-  });
+  operatorValue.addEventListener("click", addingOperatorToLastNumber);
 });
-equBtn.addEventListener("click", (e) => {
+
+equBtn.addEventListener("click", () => {
+  displayEqualAnswer();
+  //   resultMemorySave();
+});
+
+// function clearDisplay () {
+//    document.getElementById('numberBox').innerHTML = "0";
+
+//  console.log("clear pressed");
+//     }
+
+function clearStorage() {
+  localStorage.clear();
+}
+
+function addingNumbersToCalculator(e) {
+  e.preventDefault();
+  console.log("e parameter", e.target.value);
+  if (firstNumber != "" && operator != "") {
+    secondNumber = secondNumber + e.target.value;
+    expression = operator + secondNumber;
+    document.getElementById("numberBox").value = expression;
+  } else {
+    firstNumber = firstNumber + e.target.value;
+    document.getElementById("numberBox").value = firstNumber;
+    console.log(firstNumber);
+  }
+}
+
+function addingOperatorToLastNumber(e) {
+  e.preventDefault();
+  const op = e.target.value;
+  console.log(op);
+  console.log("e parameter", e.target.value);
+  if (firstNumber != "" && op != "" && secondNumber != "") {
+    firstNumber = evaluation().toFixed(3);
+    operator = firstNumber + op;
+    secondNumber = "";
+    console.log(operator);
+  } else {
+    operator = firstNumber + op;
+    document.getElementById("numberBox").value = operator;
+  }
+}
+
+function displayEqualAnswer() {
   var newResult = evaluation();
   firstNumber = newResult;
   document.getElementById("numberBox").value = newResult.toFixed(3);
   secondNumber = "";
-//   localStorage.setItem("newResult", document.getElementById('numberBox').value)
-storageSave.addEventListener('click', (e) => {
-    localStorage.setItem("newResult", document.getElementById('numberBox').value)
-})
+}
 
-});
-
+function resultMemorySave() {
+  var savedNumber = localStorage.getItem("Memory");
+  document.getElementById("numberBox").value = savedNumber;
+  console.log(savedNumber);
+}
 
 function evaluation() {
-  var firstInteger = parseFloat(expression);
-  var firstIntegerString = firstInteger.toString();
-  var firstIntegerStringlength = firstIntegerString.length;
-  let slicedExpression = expression.slice(firstIntegerStringlength);
-  var newOperator = slicedExpression[0];
-  var nextNumber = slicedExpression.slice(1);
-  var secondInteger = parseFloat(nextNumber);
+  const firstInteger = parseFloat(expression);
+  const firstIntegerString = firstInteger.toString();
+  const firstIntegerStringlength = firstIntegerString.length;
+  const slicedExpression = expression.slice(firstIntegerStringlength);
+  const newOperator = slicedExpression[0];
+  const nextNumber = slicedExpression.slice(1);
+  const secondInteger = parseFloat(nextNumber);
   let result = 0;
   if (newOperator == "+") {
     result = firstInteger + secondInteger;
@@ -91,6 +111,5 @@ function evaluation() {
   } else if (newOperator == "/") {
     result = firstInteger / secondInteger;
   }
-  
   return result;
 }
